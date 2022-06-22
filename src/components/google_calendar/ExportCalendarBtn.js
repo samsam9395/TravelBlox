609b3120fbd11c6562315df955dd08eaf394a5ce
Sammi Wang <sammimi@gmail.com>
2022-06-23 01:01:15 +0800

    fix google calendar bug and static plan title text position

--- src/components/google_calendar/ExportCalendarBtn.js
+++ src/components/google_calendar/ExportCalendarBtn.js
@@ -16,84 +16,91 @@ ExportCalendarBtn.propTypes = {
 function ExportCalendarBtn(props) {
   const [eventsToExport, setEventsToExport] = useState([]);
 
+  async function prepareEventForGoogleCalendar() {
+    const blocksListRef = collection(
+      db,
+      'plans',
+      props.planDocRef,
+      'time_blocks'
+    );
+    const docSnap = await getDocs(blocksListRef);
+    const data = docSnap.docs.map((e) => e.data());
+
+    const events = data.map((e) => ({
+      summary: e.title,
+      location: e.place_format_address,
+      description: `Place link is: ${e.place_url}`,
+      start: {
+        dateTime: new Date(e.start.seconds * 1000).toISOString(),
+      },
+      end: {
+        dateTime: new Date(e.end.seconds * 1000).toISOString(),
+      },
+      reminders: {
+        useDefault: false,
+        overrides: [
+          { method: 'email', minutes: 24 * 60 },
+          { method: 'popup', minutes: 30 },
+        ],
+      },
+    }));
+
+    setEventsToExport(events);
+  }
+
   useEffect(() => {
-    async function prepareEventForGoogleCalendar() {
-      const blocksListRef = collection(
-        db,
-        'plans',
-        props.planDocRef,
-        'time_blocks'
-      );
-      const docSnap = await getDocs(blocksListRef);
-      const data = docSnap.docs.map((e) => e.data());
-
-      const events = data.map((e) => ({
-        summary: e.title,
-        location: e.place_format_address,
-        description: `Place link is: ${e.place_url}`,
-        start: {
-          dateTime: new Date(e.start.seconds * 1000).toISOString(),
-        },
-        end: {
-          dateTime: new Date(e.end.seconds * 1000).toISOString(),
-        },
-        reminders: {
-          useDefault: false,
-          overrides: [
-            { method: 'email', minutes: 24 * 60 },
-            { method: 'popup', minutes: 30 },
-          ],
-        },
-      }));
-
-      setEventsToExport(events);
-    }
     prepareEventForGoogleCalendar();
   }, []);
 
   function handleExport() {
     const gapi = window.gapi;
+
     gapi.load('client:auth2', () => {
       const batch = gapi.client.newBatch();
 
       gapi.client.init({
         apikey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY,
+        discoveryDocs: [process.env.REACT_APP_GOOGLE_CALENDAR_DISCOVERY_DOCS],
         clientId: process.env.REACT_APP_GOOGLE_CALENDAR_CLIENT_ID,
-        discoveryDocs: process.env.REACT_APP_GOOGLE_CALENDAR_DISCOVERY_DOCS,
         scope: process.env.REACT_APP_GOOGLE_CALENDAR_SCOPE,
       });
 
-      gapi.auth2
-        .getAuthInstance()
-        .signIn()
-        .then(() => {
-          const calendar = {
-            summary: `TravelBlox: ${props.planTitle}`,
-          };
+      gapi.client.load('calendar', 'v3', () => {
+        console.log('bam! signed in ');
+        gapi.auth2
+          .getAuthInstance()
+          .signIn()
+          .then(() => {
+            const calendar = {
+              summary: `TravelBlox: ${props.planTitle}`,
+            };
 
-          const request = gapi.client.calendar.calendars.insert({
-            resource: calendar,
-          });
-
-          request.execute((calendar) => {
-            eventsToExport.forEach((event) => {
-              const request = gapi.client.calendar.events.insert({
-                calendarId: calendar.id,
-                resource: event,
-              });
-
-              batch.add(request);
+            const request = gapi.client.calendar.calendars.insert({
+              resource: calendar,
             });
-            try {
-              batch.execute((newCalendar, res) => {
-                window.open(Object.values(newCalendar)[0].result.htmlLink);
+
+            request.execute((calendar) => {
+              eventsToExport.forEach((event) => {
+                const request = gapi.client.calendar.events.insert({
+                  calendarId: calendar.id,
+                  resource: event,
+                });
+
+                batch.add(request);
               });
-            } catch (error) {
-              Swal.fire('Oops, something went wrong. Please try search again!');
-              console.log(error);
-            }
+              try {
+                batch.execute((newCalendar, res) => {
+                  window.open(Object.values(newCalendar)[0].result.htmlLink);
+                });
+              } catch (error) {
+                Swal.fire(
+                  'Oops, something went wrong. Please try search again!'
+                );
+                console.log(error);
+              }
+            });
           });
-        });
+      });
     });
   }
 
